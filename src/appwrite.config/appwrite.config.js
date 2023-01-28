0a593f312ef9ca4fbb9e94276593c0f56f98947d
jasi111 <jasimaha@gmail.com>
2023-01-28 14:06:18 +0400

    project initialsied and signup page done

---
+++ src/appwrite.config/appwrite.config.js
@@ -0,0 +1,11 @@
+import {Client, Account, Databases} from 'appwrite'
+
+const client = new Client();
+
+client.setEndpoint("http://localhost/v1").setProject("63d224c44b1afa3e6adf")
+
+export const account = new Account(client)
+
+//Database
+
+export const databases = new Databases(client, "63d2260ec050426cde61")
